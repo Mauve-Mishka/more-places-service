4b826bc72a50e496cd5043183d111cb0891f73cc
Sam Cox <sjamcox@gmail.com>
2021-02-03 21:41:13 -0800

    Add testdata.js and route for fetching mock data

--- server/app.js
+++ server/app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
 const { getPlaces } = require('./database/db.js');
+const data = require('../testdata.js');
 
 app.use(express.static('public'));
+app.use('/rooms/:id', express.static('public'));
 
 app.get('/places', async (req, res) => {
+  try {
+    res.status(200).send(data);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+app.get('/places/:id', async (req, res) => {
   try {
     const places = await getPlaces();
     res.status(200).send(places);
