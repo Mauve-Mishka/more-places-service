8dea3019d2417bea0c545a214093c188c4061769
Sam Cox <sjamcox@gmail.com>
2021-02-18 22:15:09 -0800

    Update to use getPlacesDetails and remove test route

--- server/app.js
+++ server/app.js
@@ -1,22 +1,15 @@
 const express = require('express');
 const app = express();
-const { getPlaces } = require('./database/helpers.js');
+const db = require('./database/db.js');
+const { getPlacesDetails } = require('./database/helpers.js');
 const data = require('../testdata.js');
 
 app.use(express.static('public'));
 app.use('/rooms/:id', express.static('public'));
 
-app.get('/places/test', async (req, res) => {
-  try {
-    res.status(200).send(data);
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
-});
-
 app.get('/places/:id', async (req, res) => {
   try {
-    const places = await getPlaces();
+    const places = await getPlacesDetails(req.params.id);
     res.status(200).send(places);
   } catch (err) {
     res.status(500).send(err.message);
